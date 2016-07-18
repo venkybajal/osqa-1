21f999449b64e1fc8133103dab80c42fc7bff251
Venkybajal <venkybajal@gmail.com>
2016-07-18 17:34:08 +0000

    Programming error fixed

--- forum_modules/mysqlfulltext/fts_install.sql
+++ forum_modules/mysqlfulltext/fts_install.sql
@@ -1,22 +1,19 @@
+DROP TRIGGER `fts_on_insert`;
+DROP TRIGGER `fts_on_update`;
 ALTER TABLE forum_mysqlftsindex CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-delimiter |
 
 CREATE TRIGGER fts_on_insert AFTER INSERT ON forum_node
   FOR EACH ROW
   BEGIN
     INSERT INTO forum_mysqlftsindex (node_id, title, body, tagnames) VALUES (NEW.id, UPPER(NEW.title), UPPER(NEW.body), UPPER(NEW.tagnames));
   END;
-|
 
-delimiter |
 
 CREATE TRIGGER fts_on_update AFTER UPDATE ON forum_node
   FOR EACH ROW
   BEGIN
     UPDATE forum_mysqlftsindex SET title = UPPER(NEW.title), body = UPPER(NEW.body), tagnames = UPPER(NEW.tagnames) WHERE node_id = NEW.id;
   END;
-
-|
 
 INSERT INTO forum_mysqlftsindex (node_id, title, body, tagnames) SELECT id, UPPER(title), UPPER(body), UPPER(tagnames) FROM forum_node;

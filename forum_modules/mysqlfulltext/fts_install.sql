56fac803012e7a7eb93d668435d55c14a390eeab
Venkybajal <venkybajal@gmail.com>
2016-07-20 16:40:15 +0000

    Question Search fix - but with prog errors

--- forum_modules/mysqlfulltext/fts_install.sql
+++ forum_modules/mysqlfulltext/fts_install.sql
@@ -1,19 +1,31 @@
-DROP TRIGGER `fts_on_insert`;
-DROP TRIGGER `fts_on_update`;
-ALTER TABLE forum_mysqlftsindex CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE osqa.forum_mysqlftsindex CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+delimiter |
 
-CREATE TRIGGER fts_on_insert AFTER INSERT ON forum_node
+CREATE TRIGGER osqa.fts_on_insert AFTER INSERT ON osqa.forum_node
   FOR EACH ROW
   BEGIN
-    INSERT INTO forum_mysqlftsindex (node_id, title, body, tagnames) VALUES (NEW.id, UPPER(NEW.title), UPPER(NEW.body), UPPER(NEW.tagnames));
+    INSERT INTO osqa.forum_mysqlftsindex 
+    SET node_id=NEW.id, 
+        title=UPPER(NEW.title), 
+        body=UPPER(NEW.body), 
+        tagnames=UPPER(NEW.tagnames); 
   END;
+|
 
+delimiter |
 
-CREATE TRIGGER fts_on_update AFTER UPDATE ON forum_node
+CREATE TRIGGER osqa.fts_on_update AFTER UPDATE ON forum_node
   FOR EACH ROW
   BEGIN
-    UPDATE forum_mysqlftsindex SET title = UPPER(NEW.title), body = UPPER(NEW.body), tagnames = UPPER(NEW.tagnames) WHERE node_id = NEW.id;
+    UPDATE osqa.forum_mysqlftsindex 
+    SET title = UPPER(NEW.title), 
+    body = UPPER(NEW.body), 
+    tagnames = UPPER(NEW.tagnames) 
+    WHERE node_id = NEW.id;
   END;
 
-INSERT INTO forum_mysqlftsindex (node_id, title, body, tagnames) SELECT id, UPPER(title), UPPER(body), UPPER(tagnames) FROM forum_node;
+|
+
+INSERT INTO osqa.forum_mysqlftsindex (node_id, title, body, tagnames) 
+SELECT id, UPPER(title), UPPER(body), UPPER(tagnames) FROM osqa.forum_node;
